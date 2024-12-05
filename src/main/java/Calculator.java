e3b67627b246ba5575c7b555c28d2efb791766c3
dvrnc <darryvoron1404@gmail.com>
2024-12-05 11:30:09 +0300

    Teat and add calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,46 @@
 public class Calculator {
+
+    /** * Adds two integers.
+     * @param a the first integer * @param b the second integer
+     * @return the sum of a and b */
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a+b;
     }
+
+    /** * Subtracts the second integer from the first.
+     * @param a the first integer * @param b the second integer
+     * @return the difference (a - b) */
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a-b;
     }
+
+    /** * Divides the first integer by the second.
+     * @param a the numerator * @param b the denominator
+     * @return the quotient (a / b) * @throws ArithmeticException if b is 0
+     */
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0) {
+            throw new ArithmeticException("Not zero");
+        }
+        return a/b;
     }
+    /** * Multiplies two integers.
+     * @param a the first integer * @param b the second integer
+     * @return the product of a and b */
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a*b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    /** * Solves the expression: (a + b) * (c - d) / d
+     * @param a the first integer
+     * @param b the second integer
+     * @param c the third integer
+     * @param d the fourth integer
+     * @return the result of the expression */
+    public int solver(int a, int b, int c, int d){
+        int sum = add(a,b);
+        int diff = dif(c,d);
+        int time = times(sum,diff);
+        return div(time,d);
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,26 +6,39 @@ class CalculatorTest {
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Calculator calculator = new Calculator();
+        assertEquals(8,calculator.add(5,3));
+        assertEquals(-1,calculator.add(-2,1));
+        assertEquals(3,calculator.add(2,1));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Calculator calculator = new Calculator();
+        assertEquals(9,calculator.dif(11,2));
+        assertEquals(4,calculator.dif(8,4));
+        assertEquals(3,calculator.dif(4,1));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Calculator calculator = new Calculator();
+        assertEquals(4,calculator.div(8,2));
+        assertEquals(1,calculator.div(7,7));
+        assertEquals(9,calculator.div(81,9));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Calculator calculator = new Calculator();
+        assertEquals(18,calculator.times(9,2));
+        assertEquals(49,calculator.times(7,7));
+        assertEquals(42,calculator.times(7,6));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calculator = new Calculator();
+        assertEquals(36,calculator.solver(5,4,10,2));
     }
 }
