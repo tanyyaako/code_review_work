fa25aa3e82d6161cbac32655cf0f1df07d540340
tanyyaako <tanyyaako@icloud.com>
2024-12-05 11:29:54 +0300

    Add calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,57 @@
 public class Calculator {
+    /**
+     * Adds two integers.
+     * @param a the first integer
+     * @param b the second integer
+     * @return the sum of a and b
+     */
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a+b;
     }
+    /**
+     * Subtracts the second integer from the first.
+     * @param a the first integer
+     * @param b the second integer
+     * @return the difference (a - b)
+     */
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a-b;
     }
+    /**
+     * Divides the first integer by the second.
+     * @param a the numerator
+     * @param b the denominator
+     * @return the quotient (a / b)
+     * @throws ArithmeticException if b is 0
+     */
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if(b==0){
+            System.out.println(" b cannot be 0");
+        }
+        return a/b;
     }
+    /**
+     * Multiplies two integers.
+     * @param a the first integer
+     * @param b the second integer
+     * @return the product of a and b
+     */
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a*b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    /**
+     * Solves the expression: (a + b) * (b - a) / c
+     * @param a the first integer
+     * @param b the second integer
+     * @param c the third integer (denominator in division)
+     * @return the result of the expression
+     * @throws ArithmeticException if c is 0
+     */
+    public int solver(int a,int b,int c){
+        int sum = add(a,b);
+        int dif = dif(b,a);
+        int times = sum*dif;
+        int div = div(times,c);
+        return div;
     }
 }
